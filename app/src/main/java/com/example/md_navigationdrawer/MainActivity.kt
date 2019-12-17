713a7c0f0cf08219616e2cb18d6ddb0f9e1ad9bf
unknown <phamdat08051998@gmail.com>
2019-12-17 16:07:03 +0700

    first commit

---
+++ app/src/main/java/com/example/md_navigationdrawer/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.md_navigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.annotation.NonNull
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_navigation_layout.*
+
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener,MyAdapter.OnClickItem {
+    var list : MutableList<model> = mutableListOf()
+    lateinit var adapter : MyAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        createNavigationDawer()
+
+        createRecyclerView()
+    }
+
+    fun createRecyclerView(){
+        createData()
+        recyler.layoutManager = LinearLayoutManager(this)
+        recyler.setHasFixedSize(true)
+        adapter = MyAdapter(list,this)
+        recyler.adapter=adapter
+
+    }
+
+    fun createData(){
+        //val model1: model? =null
+
+        for(i in 1..10){
+            val model = model(i.toString(),"hjgasfgasfgyasdjfgsadjhfgasdhfhgasjfjvhasjfgvasdjfvasdfvhasdfha","a","a","a","a","a","a")
+            list.add(model)
+        }
+    }
+    fun createNavigationDawer(){
+        toolbar.title="Việt Nam"
+
+        val toggle=ActionBarDrawerToggle(this@MainActivity,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close)
+        navigation.setNavigationItemSelectedListener(this)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        var message = p0.title
+        Toast.makeText(applicationContext,"$message",Toast.LENGTH_SHORT).show()
+
+//        if (p0.isChecked()) {
+//            p0.setChecked(false)
+//        }
+//        else
+//            p0.setChecked(true)
+        //p0.setChecked((true))
+        closeDrawer()
+        when(p0.itemId){
+            R.id.daBong ->{
+
+            }
+            R.id.bongChuyen ->{}
+            R.id.bongBan ->{}
+            R.id.bongTo ->{}
+            R.id.cauLong ->{}
+            R.id.chayBo ->{}
+            R.id.daCau ->{}
+            R.id.nhayCau ->{}
+            R.id.nhayDay ->{}
+        }
+        closeDrawer()
+        return false
+    }
+
+    private fun closeDrawer() {
+        drawerLayout.closeDrawer(GravityCompat.START)
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            closeDrawer()
+        }else{
+            super.onBackPressed()
+        }
+    }
+
+    override fun onClick(model: model) {
+        Toast.makeText(this,"${model.s1}  ${model.s2}",Toast.LENGTH_SHORT).show()
+    }
+
+}
---
+++ app/src/main/java/com/example/md_navigationdrawer/MyAdapter.kt
@@ -0,0 +1,80 @@
+package com.example.md_navigationdrawer
+
+import android.annotation.SuppressLint
+import android.content.ClipData
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item.view.*
+
+class MyAdapter(var list : MutableList<model>,var listener:OnClickItem):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private  var oldView:CardView? = null
+//    private val TYPE_HEADER = 1
+//    private val TYPE_ITEM = 2
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+//        if(viewType==TYPE_HEADER){
+//            val view:View = LayoutInflater.from(parent.context).inflate(R.layout.header_row,parent,false)
+//            return MyViewHolder1(view)
+//        }else{
+            val view:View = LayoutInflater.from(parent.context).inflate(R.layout.item,parent,false)
+            return MyViewHolder2(view)
+//        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder is MyViewHolder2){
+            holder.bind(list.get(position),listener)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+//    override fun getItemViewType(position: Int): Int {
+//        if(checkHeader(position)){
+//            return TYPE_HEADER
+//        }else{
+//            return TYPE_ITEM
+//        }
+//    }
+
+//    private fun checkHeader(possition: Int):Boolean{
+//        if(possition==0){
+//            return true
+//        }
+//        return false
+//    }
+    inner class MyViewHolder2(var item1: View): RecyclerView.ViewHolder(item1) {
+        @SuppressLint("ResourceAsColor")
+        fun bind(model:model, listener: OnClickItem){
+            item1.boq_label.text = model.s1
+            item1.boq_title.text = model.s2
+            item1.boq_unit.text = model.s3
+            item1.boq_quantity.text = model.s4
+            item1.boq_rate.text = model.s5
+            item1.rateIn.text = model.s6
+            item1.amo.text = model.s7
+            item1.remarks.text = model.s8
+            item1.setOnClickListener {
+                if(oldView!=null) {
+                    oldView?.setCardBackgroundColor(Color.WHITE)
+                }
+                item1.cardView.setCardBackgroundColor(R.color.colorPrimary)
+                oldView=item1.cardView
+                listener.onClick(model)
+            }
+        }
+    }
+
+//    inner class MyViewHolder1(item:View):RecyclerView.ViewHolder(item) {
+//    }
+
+    interface OnClickItem{
+        fun onClick(model: model)
+    }
+
+}
